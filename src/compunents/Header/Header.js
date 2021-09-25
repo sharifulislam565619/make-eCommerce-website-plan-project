ae6cc66c9ab2f435efc8c850e69ab3665e5a21af
sharifulislam565619 <sharifulislam565619@gmail.com>
2021-09-25 23:36:01 +0600

    first commit

---
+++ src/compunents/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Make an E-commerce website</h1>
+        </div>
+    );
+};
+
+export default Header;
